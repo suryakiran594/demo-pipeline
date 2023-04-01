8b8ea9fcbbd1116edc817d740622e19ef28475bd
suryakiran594 <120645302+suryakiran594@users.noreply.github.com>
2023-04-01 07:53:20 +0530

    all files added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="census-income",
+      version="0.0.1",
+      author="surya",
+      author_mail="suryakiran594@yahoo.com",
+      packages=find_packages(),
+     install_requires=["pandas","numpy","flask"]
+      )
